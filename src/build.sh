89cba34a2b034a8097e043e293222c0c0be51839
hooklife <miaogaopeng123@gmail.com>
2021-04-09 10:28:32 +0800

    Init

---
+++ src/build.sh
@@ -0,0 +1,16 @@
+set -e
+
+REPO=hooklife/ocatane
+
+function build_and_push(){
+    ARGS="--build-arg SW_VERSION=v${SWOOLE_VERSION} --build-arg VERSION=${VERSION}"
+	docker build ${ARGS} -t ${REPO}:8.0-swoole-${SWOOLE_VERSION}-${VERSION} --target main .
+	docker build ${ARGS} -t ${REPO}:8.0-swoole-${SWOOLE_VERSION}-${VERSION}-dev --target dev .
+
+    docker push ${REPO}:8.0-swoole-${SWOOLE_VERSION}-${VERSION}
+    docker push ${REPO}:8.0-swoole-${SWOOLE_VERSION}-${VERSION}-dev
+}
+
+export SWOOLE_VERSION=4.6.4
+export VERSION=0.0.1
+build
